024aadd92525e9acd2edf04973b08b710b80c6cd
Krishna Vadavi <krishnavadavi@krishnas-mini.lan>
2022-11-09 09:16:10 -0600

    updates

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,10 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-let criteria = ["length", "upperCase", "lowerCase", "specialCharacter", "numeric"]
 let lowerCaseAlphabets = "abcdefghijklmnopqrstuvwxyz"
 let upperCaseAlphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 let numbers = "0123456789"
-let specialCharacters = ["!", "@", "#", "$", "&", "+", "_"]
+let specialCharacters = ["!", "@", "#", "$", "&", "+", "_", "%", "?"]
 
 let passwordLength = ""
-let passwordLower = ""
-let passwordUpper = ""
-let passwordSpecialChar = ""
-let passwordFinal = ""
 
 let randomLowerAlpha = []
 let randomUpperAlphaConcat
@@ -24,6 +17,8 @@ let randomNumeric = []
 let randomNumSpecUppLowConcat = []
 let randomFinal = []
 
+var generateBtn = document.querySelector("#generate");
+
 function getRandomInt(n) {
   return Math.floor(Math.random() * n);
 }
@@ -32,22 +27,17 @@ function getRandomInt(n) {
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = randomFinal;
-
 }
 
 function generatePassword() {
-  console.log("Button hit")
   window.alert("Select Password Creteria: \nLength, Numeric, UpperCase, LowerCase and Special Character:");
   passLengthFunc()
   // If user pressed Cancel, immediately end function
 }
 
 function passLengthFunc() {
-  
   var passLength = window.prompt("Enter length of password. \nLength of at least 8 characters and no more than 128 characters:");
-  
   if (!passLength) {
     return;
   }
@@ -58,22 +48,16 @@ function passLengthFunc() {
 
   } else {
     passwordLength = passLength
-    console.log(passwordLength)
     passLowerFunc()
   }
-
 }
 
 function passLowerFunc() {
-  
   let passLower = window.prompt("Include Lower Case Characters? \nYES or NO:");
-  
   if (!passLower) {
     return;
   }
-
   passLower = passLower.toUpperCase()
-  console.log(passLower)
 
   if (passLower === "YES") {
     console.log("YES working")
@@ -81,36 +65,24 @@ function passLowerFunc() {
     for (i=0; i<passwordLength; i++) {
       randomLowerAlpha += lowerCaseAlphabets[Math.floor(Math.random() * lowerCaseAlphabets.length)]
     }
-    
-    console.log(randomLowerAlpha)
-    passwordLower = randomLowerAlpha
-  
     passUpperFunc()
   } 
   
   if (passLower === "NO") {
-    console.log("No");
     passUpperFunc()
   } 
 }
 
-
-
-
 function passUpperFunc() {
-  
   var passUpper = window.prompt("Include Upper Case Characters? \nYES or NO:");
-  
   if (!passUpper) {
     return;
   }
 
   passUpper = passUpper.toUpperCase()
-  console.log(passUpper)
 
   if (passUpper === "YES") {
     let i
-
     for (i=0; i<passwordLength; i++) {
       randomUpperAlpha += upperCaseAlphabets[Math.floor(Math.random() * upperCaseAlphabets.length)]
     }
@@ -118,69 +90,50 @@ function passUpperFunc() {
     passSpecialFunc()
   } 
   if (passUpper === "NO") {
-    console.log("No");
     passSpecialFunc()
   } 
 }
 
 
-
-
 function passSpecialFunc() {
-  
   var passSpecial = window.prompt("Include Special Characters? \nYES or NO:");
-  
   if (!passSpecial) {
     return;
   }
-  console.log(passSpecial)
   passSpecial = passSpecial.toUpperCase()
 
   if (passSpecial === "YES") {
     let i
-    let j
-
     for (i=0; i<passwordLength; i++){
       randomSpecialAlpha += specialCharacters[Math.floor(Math.random() * specialCharacters.length)]
-
     }
-
-    // randomSpecUppLowConcat = randomSpecialAlpha + randomUpperLower
     randomSpecUppLowConcat = randomSpecialAlpha + randomUpperAlphaConcat
-    console.log(randomSpecUppLowConcat)
     passNumericFunc()
   } 
   if (passSpecial === "NO") {
-    console.log("No");
     passNumericFunc()
   } 
 }
 
 function passNumericFunc() {
-  
   var passNumeric = window.prompt("Include Numbers? \nYES or NO:");
-  
   if (!passNumeric) {
     return;
   }
-  console.log(passNumeric)
   passNumeric = passNumeric.toUpperCase()
 
   if (passNumeric === "YES") {
     let i
     for (i=0; i<passwordLength; i++){
       randomNumeric += numbers[Math.floor(Math.random() * numbers.length)]
-
     }
 
     randomNumSpecUppLowConcat = randomNumeric + randomSpecUppLowConcat
-    console.log(randomNumSpecUppLowConcat)
 
     let j
     for (j=0; j<passwordLength; j++) {
       randomFinal += randomNumSpecUppLowConcat[Math.floor(Math.random() * randomNumSpecUppLowConcat.length)]
     }
-    console.log(randomFinal)
     validatePass()
   } 
   if (passNumeric === "NO") {
@@ -188,7 +141,6 @@ function passNumericFunc() {
     validatePass()
   } 
 }
-
 
 function validatePass() {
   const validatePass = new RegExp('abcdefghijklmnopqrstuvwxyz')
